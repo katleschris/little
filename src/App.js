5f4e25f2913080c140fc20a54ffdb6bf098bf350
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-02-04 16:12:24 +0200

    set semantic foundations of application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
+import React from 'react'
 import './App.css';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      Little Lemon Website
-    </div>
+    <>
+    <Nav />
+    <Header />
+    <Main />
+    <Footer />
+    </>
   );
 }
 
