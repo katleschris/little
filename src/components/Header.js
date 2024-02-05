bcd92840b51b2271f1c10b3be15176731226ed5f
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-02-05 22:28:46 +0200

    Added and styled icons in Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import Nav from './Nav';
-import Logo from '../icons_assets/Logo .svg';
+import Logo from '../icons_assets/Logo.svg';
 import Basket from '../icons_assets/Basket.svg';
 import HamburgerIcon from '../icons_assets/HamburgerIcon.svg';
 
 export default function Header() {
+  const headerStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gap: '10px',
+    alignItems: 'center',
+    padding: '10px',
+  };
+
+  const basketStyle = {
+    justifySelf: 'end',
+  };
+
   return (
-    <header>
+    <header style={headerStyle}>
       <img src={HamburgerIcon} alt='navigation Icon' />
       <img src={Logo} alt='Little Lemon Logo' />
-      <img src={Basket} alt='cart' />
+      <img src={Basket} alt='cart' style={basketStyle} />
+      <Nav />
     </header>
   );
 }
