6ae169003aa7d5faa6b8b5c95a67915e8ec9ed96
Katlego Christabel Leshiba <katlegochristabelleshiba@gmail.com>
2024-02-05 22:18:33 +0200

    placed assets in src folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Nav from './Nav';
+import Logo from '../icons_assets/Logo .svg';
+import Basket from '../icons_assets/Basket.svg';
+import HamburgerIcon from '../icons_assets/HamburgerIcon.svg';
 
 export default function Header() {
   return (
-    <>
-      <img src="..\public\Logo.svg" alt='little lemon logo'/>
-      <h1>Header</h1>
-    </>
-  )
+    <header>
+      <img src={HamburgerIcon} alt='navigation Icon' />
+      <img src={Logo} alt='Little Lemon Logo' />
+      <img src={Basket} alt='cart' />
+    </header>
+  );
 }
